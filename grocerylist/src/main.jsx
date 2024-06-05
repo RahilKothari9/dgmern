4d48242f86768833e09086393ccd3f8b19678fee
Rahil Kothari <kotharirahil9@gmail.com>
2024-06-05 11:46:48 +0530

    Add vercel analytics

--- grocerylist/src/main.jsx
+++ grocerylist/src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { CategoryProvider } from './contexts/CategoryContext.jsx'
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CategoryProvider>
       <App />
+      <Analytics/>
     </CategoryProvider> 
   </React.StrictMode>,
 )
